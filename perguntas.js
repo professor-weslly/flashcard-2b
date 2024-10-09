8582eaff389854428b5214e937c94fb8d1ece8a4
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-10-09 09:13:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,35 @@
 criaCartao(
-    'Título',
-    'Pergunta',
-    'Resposta'
+    'Phyton',
+    'O que é phyton?',
+    'O phyton é uma linguagem de programação'
 )
 criaCartao(
-    'Batata',
-    'Você gosta de batata?',
-    'Batatas são legais'
+    'Site',
+    'Html é usado para que?',
+    'Criação de Sites'
 )
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'Paris'
+    'PHP',
+    'O que é PHP?',
+    'O PHP é uma linguagem de programação'
+)
+criaCartao(
+    'C#',
+    'C# é uma linguagem de programação, multiparadigma,?',
+    'O phyton é uma linguagem de programação'
+)
+criaCartao(
+    'multiparadigma',
+    'O que é multiparadigma?',
+    'a linguagem multiparadigma nada mais é a programação que suporta vários paradigmas de programação'
+)
+criaCartao(
+    'C++',
+    'O que é C++?',
+    'é uma linguagem de programação compilada multi-paradigma e de uso geral'
+)
+criaCartao(
+    'Assembly',
+    'O que é linguagem assembly?',
+    ' é uma linguagem de programação de baixo nível'
 )
